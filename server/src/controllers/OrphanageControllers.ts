086f23f04cbc291e47d837af10cf8f4b1ae44fb2
Hiaguedes <hiago_guedes94@hotmail.com>
2020-10-13 16:24:47 -0300

    listagem de orfanatos e mostrar orfanato com id específico

--- server/src/controllers/OrphanageControllers.ts
+++ server/src/controllers/OrphanageControllers.ts
@@ -11,5 +11,20 @@ export default {
         const orphanages = orphanagesRepository.create({name, latitude,longitude,about,instructions,opening_hours,open_on_weekends: Boolean(open_on_weekends)})
         await orphanagesRepository.save(orphanages);
         return response.status(201).json(orphanages)
+    },
+
+    async index(request: Request,response: Response){
+        const orphanagesRepository = getRepository(Orphanage);
+        const orphanages = await orphanagesRepository.find();
+
+        return response.json(orphanages);
+    },
+
+    async show(request: Request,response: Response){
+        const {id} = request.params;
+        const orphanagesRepository = getRepository(Orphanage);
+        const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+        return response.json(orphanage);
     }
 }
