f901190eb1d5a4d352491f4b7e3e1ea99d694b57
Hiaguedes <hiago_guedes94@hotmail.com>
2020-10-13 16:14:38 -0300

    organizando o server no padrão MVC

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,15 @@
 import express from 'express';
 import './database/connection';
-import {getRepository} from 'typeorm';
-import Orphanage from './models/Orphanages';
+import routes from './models/routes'
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(routes)
 
 // request -> requisição do front end
 //response -> resposta do back end
 
-app.post('/orphanages', async (request, response) => {
-    
-    const {name, latitude,longitude,about,instructions,opening_hours,open_on_weekends} =request.body; 
-    
-    const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanages = orphanagesRepository.create({name, latitude,longitude,about,instructions,opening_hours,open_on_weekends: Boolean(open_on_weekends)})
-    await orphanagesRepository.save(orphanages);
-    return response.status(201).json(orphanages)
-});
 
 // método json para envio de objetos para ser consumida pois estamos criando API
 
